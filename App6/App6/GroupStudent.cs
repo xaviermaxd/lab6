1455e412ede6d78204464d921e14e7aadad89251
Xavier Mamani <xavier.mamani@tecsup.edu.pe>
2023-10-14 18:50:51 -0500

    Dividir por seccion

---
+++ App6/App6/GroupStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace App6
+{
+    public class GroupStudent<K, T> : ObservableCollection<T>
+    {
+        public K Key { get; private set; }
+
+        public GroupStudent(K key, IEnumerable<T> items)
+        {
+            Key = key;
+            foreach (var item in items)
+                this.Items.Add(item);
+        }
+    }
+}
--- App6/App6/MainPage.xaml.cs
+++ App6/App6/MainPage.xaml.cs
@@ -13,15 +13,6 @@ namespace App6
         public MainPage()
         {
             InitializeComponent();
-            List<string> estudiantes = new List<string>();
-            for (int i = 0; i < 20; i++)
-            {
-                estudiantes.Add("Estudiante " + i.ToString());
-            }
-            //lvEstudiantes.ItemsSource = estudiantes;
-
-
-
             List<Estudiante> estudiantes1= new List<Estudiante>();
             for (int i = 0; i < 10; i++)
             {
@@ -35,29 +26,11 @@ namespace App6
                     }
                     ); ;
             }
-            var groupedStudents = estudiantes1.GroupBy(e => e.Seccion);
+            
 
-            //lvEstudiantes1.ItemsSource = groupedStudents;
+            var groupedStudents = estudiantes1.GroupBy(e => e.Seccion).Select(g => new GroupStudent<string, Estudiante>(g.Key, g));
+
             lvEstudiantes1.ItemsSource = groupedStudents;
-            lvEstudiantes1.IsGroupingEnabled = true;
-            lvEstudiantes1.GroupDisplayBinding = new Binding("Key"); // Esto mostrará la sección como el encabezado del grupo
-
-            //lvEstudiantes1.ItemTemplate = new DataTemplate(typeof(TextCell)); // Puedes personalizar esto como quieras
-            //lvEstudiantes1.ItemTemplate.SetBinding(TextCell.TextProperty, "Nombres");
-            //lvEstudiantes1.ItemTemplate.SetBinding(TextCell.TextProperty, "Apellidos");
-            //lvEstudiantes1.ItemTemplate.SetBinding(TextCell.TextProperty, "Edad");
-            lvEstudiantes1.ItemTemplate = new DataTemplate(() =>
-            {
-                // Crea una nueva celda
-                var cell = new TextCell();
-
-                // Configura los bindings para Nombres, Apellidos y Edad
-                cell.SetBinding(TextCell.TextProperty, new Binding("Nombres"));
-                cell.SetBinding(TextCell.DetailProperty, new Binding("Apellidos"));
-                cell.SetBinding(TextCell.DetailColorProperty, new Binding("Edad"));
-
-                return cell;
-            });
 
         }
     }
